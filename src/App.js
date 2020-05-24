47069cd789cd996678a74e81561c7ff66be3ab74
Bshal <bishalchettri408@gmail.com>
2020-05-24 08:02:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Navbar from './components/layouts/Navbar';
+import Home from './components/pages/Home';
+import SingleUser from './components/users/most_followed_users/SingleUser';
+import MostFollowedUsersPage from './components/users/most_followed_users/MostFollowedUsersPage';
+import MostStarredRepositoriesPage from './components/repos/MostStarredRepositoriesPage';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+
 import './App.css';
 
-function App() {
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/user/:login" component={SingleUser} />
+          <Route exact path="/users/:users" component={MostFollowedUsersPage} />
+          <Route exact path="/repos/:repos" component={MostStarredRepositoriesPage} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/repos/MostStarredRepositoriesPage.js
@@ -0,0 +1,50 @@
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+import MostStarredRepository from './MostStarredRepository';
+import Spinner from '../layouts/Spinner';
+
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production'){
+	githubClientId = process.env.REACT_APP_GITHUB_CLIEND_ID;
+	githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+}else{
+	githubClientId = process.env.GITHUB_CLIEND_ID;
+	githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
+const MostStarredRepositoriesPage = () =>{
+	const [most_starred_repositories,set_most_starred_repositories] = useState([]);
+	const [loading,set_loading] = useState(false);
+
+	useEffect(() => {
+		set_loading(true);
+		const fetchData = async () => {
+			const response = await axios.get(`https://api.github.com/search/repositories?q=stars:>=500&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
+			(response.data.items) && set_loading(false);
+			set_most_starred_repositories(response.data.items);
+		}
+
+		fetchData();
+		//eslint-disable-next-line
+
+	},[])
+
+	return(
+		<div className="main">
+			<div className="main_container">
+				<h1 className="heading_sub underline">
+					Most Starred Repositories on Github:
+				</h1>
+				{loading && <Spinner />}
+				<ul className="main_cards-block">
+					{most_starred_repositories.map(repo => <MostStarredRepository key={repo.id} repo={repo} />)}
+				</ul>
+			</div>
+		</div>
+	)
+
+}
+
+export default MostStarredRepositoriesPage;
